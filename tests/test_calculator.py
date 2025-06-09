0490824b168b622ed6accaea8d1de17506330d76
Rahul Patil <rahulpatil18@users.noreply.github.com>
2025-06-09 17:40:40 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,9 @@
 # tests/test_calculator.py
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
+
+# Add project root to PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from calculator import add, subtract
 
