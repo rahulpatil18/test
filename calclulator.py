cb50ad0cda806a1e6dff3056e31501d4834c5022
Rahul Patil <rahulpatil18@users.noreply.github.com>
2025-06-09 17:24:19 +0530

    Update calclulator.py

--- calclulator.py
+++ calclulator.py
@@ -1,4 +1,10 @@
 # calculator.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
+
+from calculator import add, subtract
+
 def add(a, b):
     return a + b
 
